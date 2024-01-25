5047b7c8b53cb8492b48575df8cd07e43ccad25a
Oleh Bohatov <obohatov@gmail.com>
2024-01-25 16:09:10 +0100

    finish table and openspace logic

--- src/openspace.py
+++ src/openspace.py
@@ -1,11 +1,20 @@
 import table
 import random
 
+
 class OpenSpace:
     """This class represents an open space with a list of tables"""
 
-    def __init__(self, number_of_tables):
+    filename = "result.xlsx"
+
+    def __init__(self, number_of_tables = 6):
         self.tables = [table.Table(4) for _ in range(number_of_tables)]
+
+    def __str__(self) -> str:
+        places = []
+        for table in self.tables:
+            places.append(f"{table.capacity}: {table.seats}")
+        return f"Open space with {len(self.tables)} tables ({places})"
 
     def __str__(self) -> str:
         return f"{self.tables}"
@@ -18,4 +27,17 @@ class OpenSpace:
                 if table.has_free_spot():
                     table.assign_seat(name)
                     break
-  
+
+    def display(self):
+        """Display the tables and students"""
+        for i, table in enumerate(self.tables, start=1):
+            print(f"Table {i}:")
+            for seat in table.seats:
+                if seat.free:
+                    print("\tEmpty")
+                else:
+                    print(f"\tOccupied by {seat.occupant}")
+                    
+    def store(self, filename):
+        """Store the repartition in Excel file"""
+        pass  
--- src/table.py
+++ src/table.py
@@ -3,31 +3,46 @@ class Seat:
     
     def __init__(self):
         self.free = True
-        self.student = None
+        self.occupant = None
 
     def __str__(self) -> str:
-        return f"{self.free} {self.student}"
+        return f"This seat are free: {self.free} {self.occupant}"
 
-    def set_student(self, name):
+    def set_occupant(self, name):
         """Assign a seat if it's free"""
         if self.free:
-            self.student = name
+            self.occupant = name
             self.free = False
 
-    def remove_student(self):
+    def remove_occupant(self):
         """Remove a student and return the student's name"""
-        student = self.student
-        self.student = None
+        occupant = self.occupant
+        self.occupant = None
         self.free = True
-        return student
+        return occupant
 
 class Table:
     """This class represents a table with a list of seats"""
     
-    def __init__(self, capacity):
+    def __init__(self, capacity = 4):
         self.capacity = capacity
         self.seats = [Seat() for _ in range(capacity)]
     
     def __str__(self) -> str:
         return f"{self.capacity} {self.seats}"
+    
+    def has_free_spot(self):
+        """Check is the table has free spot""" 
+        return any(seat.free for seat in self.seats)
+
+    def assign_seat(self, name):
+        """Assign someone to the first available seat in the table"""
+        for seat in self.seats:
+            if seat.free:
+                seat.set_occupant(name)
+                return
+
+    def capacity_left(self):
+        """Return the number of free seats"""
+        return len([seat for seat in self.seats if seat.free])
     
